6d4f632d7ba70a6ade7348ef6150ed4676892eb6
Petromir Kolev <petromirkolev@gmail.com>
2022-10-06 20:44:49 +0300

    Add rainbow drawing mode

--- script.js
+++ script.js
@@ -6,24 +6,55 @@ const gridElement = '<div class="grid-element"></div>';
 const gridTemplate = 'auto ';
 const increaseSliderButton = document.querySelector('#increase');
 const decreaseSliderButton = document.querySelector('#decrease');
-const colorButton = document.querySelector('.color-mode');
-const rainbowButton = document.querySelector('.rainbox-mode');
-const grayscaleButton = document.querySelector('.grayscale-mode');
-const eraseButton = document.querySelector('.eraser-mode');
-const clearGridButton = document.querySelector('.clear-mode');
-const randomSketchButton = document.querySelector('.random-sketch-mode');
 const controlPanel = document.querySelector('.controls');
+const randomColors = [
+   '#ff0000',
+   '#ffa500',
+   '#ffff00',
+   '#008000',
+   '#0000ff',
+   '#4b0082',
+   '#ee82ee',
+];
 let currentMode;
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Game logic
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Game logic //
+
 // Update the sketch grid based on slider position
 const updateSketchGrid = function () {
    playingField.style.gridTemplateRows = gridTemplate.repeat(slider.value);
    playingField.style.gridTemplateColumns = gridTemplate.repeat(slider.value);
    playingField.innerHTML = gridElement.repeat(slider.value * slider.value);
 };
-// Check for grid element click & hover and do something
+
+// Set color to rainbow colors
+const rainbowColor = function () {
+   return randomColors[Math.trunc(Math.random() * randomColors.length)];
+};
+
+const drawingMode = function () {
+   switch (currentMode) {
+      case 'btn-black':
+         return 'black';
+      case 'btn-grayscale':
+         return 'gray';
+      case 'btn-rainbow':
+         return rainbowColor();
+      case 'btn-clear':
+         return clearGrid();
+      case 'btn-random':
+         return randomSketch();
+      case 'btn-erase':
+         1;
+         break;
+      default:
+         return 'gray';
+   }
+};
 
 // Event listeners
 // Increase slider value by button
@@ -40,15 +71,17 @@ decreaseSliderButton.addEventListener('click', () => {
 slider.addEventListener('input', function () {
    updateSketchGrid();
 });
-// e.buttons = 1 if mouse clicked when hover
+
+// Add color to grid element when mouse click and over
 playingField.addEventListener('mouseover', function (e) {
+   const gridBox = e.target.style;
    if (e.buttons !== 1 || !e.target.classList.contains('grid-element')) {
       return;
    } else {
-      e.target.style.backgroundColor = 'red';
+      gridBox.backgroundColor = drawingMode();
    }
 });
-
+// Handle menu buttons click and store current button class as a variable
 controlPanel.addEventListener('click', function (e) {
    if (!e.target.classList.contains('btn')) return;
 
@@ -57,8 +90,7 @@ controlPanel.addEventListener('click', function (e) {
       e.classList.remove('btn-clicked');
    });
    e.target.classList.add('btn-clicked');
-   currentMode = e.target;
-   console.log(currentMode);
+   currentMode = e.target.classList[1];
 });
 
 updateSketchGrid();
